b81b05ebd7e6763a26bb1621f52e7fa2a8154b87
jeikei-anhhoang1420 <lehuuhoanganhhg2003@gmail.com>
2023-08-25 11:43:35 +0700

    header

--- public/css/store.js
+++ public/css/store.js
@@ -7,16 +7,88 @@ let products = {
             image: "img/61Ep+91fCCL._AC_SX679._SX._UX._SY._UY_.jpg"
         },
         {
-            productname: "Dickies Men's Heavyweight Crew Neck Short Sleeve Tee",
-            category: "Shirts",
-            price: "16",
-            image: "img/5183zo0U+2L._AC_UX679_.jpg"
+            productname: "Amazon Essentials Men's Classic-Fit Casual Stretch Khaki Pant",
+            category: "Trousers",
+            price: "44",
+            image: "img/71Em6nho+JL._AC_UX569_.jpg"
         },
         {
             productname: "Amazon Essentials Men's Hooded Fleece Sweatshirt (Available in Big & Tall)",
             category: "Hoodies",
-            price: "16",
+            price: "18",
             image: "img/71vV02rloNL._AC_UY741_.jpg"
         },
+        {
+            productname: "Haggar Men's Cool 18 Pro Straight Fit Flat Front Superflex Waistband Pant",
+            category: "Trousers",
+            price: "37",
+            image: "img/61tm4WT9JNL._AC_UX679_.jpg"
+        },
+        {
+            productname: "Outdoor Cap Mountain Dad Hat - Unstructured Soft Cotton Cap",
+            category: "Headwear",
+            price: "13",
+            image: "img/81Y5SylHdYL._AC_UX679_.jpg"
+        },
+        {
+            productname: "Dickies Men's Heavyweight Crew Neck Short Sleeve Tee",
+            category: "Shirts",
+            price: "23",
+            image: "img/5183zo0U+2L._AC_UX679_.jpg"
+        },
+        {
+            productname: "Lucky Brand Men's 181 Relaxed Straight Jean",
+            category: "Trousers",
+            price: "57",
+            image: "img/91VRRD4DLdL._AC_UX569_.jpg"
+        },
+        {
+            productname: "Nike Men's Basketball Shoes",
+            category: "Shoes",
+            price: "180",
+            image: "img/71+Z1jCoXnL._AC_UX695_.jpg"
+        },
+        {
+            productname: "BOSS Men's Cotton Twill Small Logo Cap",
+            category: "Headwear",
+            price: "23",
+            image: "img/81DO7sFb31L._AC_UX679_.jpg"
+        },
+        {
+            productname: "NIKE Men's Basketball Shoes",
+            category: "Shoes",
+            price: "159",
+            image: "img/71blOkj9gLL._AC_UX695_.jpg"
+        },
+        {
+            productname: "Razer Xanthus Tote Bag: Dedicated Padded Laptop Compartment - Fits up to 16 Laptops – Water Repellent Nylon - Two-Way Carry with Detachable Shoulder Strap",
+            category: "Bags",
+            price: "89",
+            image: "img/815VouNV98L._AC_SX679_.jpg"
+        },
+        {
+            productname: "PS by Paul Smith Mens Unlined Coach Jacket",
+            category: "Jackets",
+            price: "450",
+            image: "img/71pefASy-CL._AC_UX679_.jpg"
+        },
+        {
+            productname: "Pete The Cat Stickers",
+            category: "Stickers",
+            price: "6",
+            image: "img/81c2RWSh7zL._AC_SX466_.jpg"
+        },
+        {
+            productname: "PUMA Kids' Evercat Transformation Duffel",
+            category: "Bags",
+            price: "20",
+            image: "img/A1dgKQwBs2L._AC_UX679_.jpg"
+        },
+        {
+            productname: "READY 2 LEARN Foam Stickers",
+            category: "Stickers",
+            price: "7",
+            image: "img/918SlGD1KbL._SX522_.jpg"
+        }
     ]
 };
