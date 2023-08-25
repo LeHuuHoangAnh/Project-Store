6da9232c90de1841105afbfeef4bc893d703c55e
jeikei-anhhoang1420 <lehuuhoanganhhg2003@gmail.com>
2023-08-25 10:24:04 +0700

    Header

---
+++ public/css/store.js
@@ -0,0 +1,22 @@
+let products = {
+    data: [
+        {
+            productname: "Dickies Men's Heavyweight Crew Neck Short Sleeve Tee",
+            category: "Shirts",
+            price: "16",
+            image: "img/61Ep+91fCCL._AC_SX679._SX._UX._SY._UY_.jpg"
+        },
+        {
+            productname: "Dickies Men's Heavyweight Crew Neck Short Sleeve Tee",
+            category: "Shirts",
+            price: "16",
+            image: "img/5183zo0U+2L._AC_UX679_.jpg"
+        },
+        {
+            productname: "Amazon Essentials Men's Hooded Fleece Sweatshirt (Available in Big & Tall)",
+            category: "Hoodies",
+            price: "16",
+            image: "img/71vV02rloNL._AC_UY741_.jpg"
+        },
+    ]
+};
